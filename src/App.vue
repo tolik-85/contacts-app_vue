9033983666d33f3374c2fffd60c4f5788d8fe2f9
tolik-85 <tolian8576@gmail.com>
2025-04-05 22:12:05 +0300

    remove comments

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,8 @@ export default {
   data() {
     return {
       contacts: [],
-      recentCalls: [], // recentCalls
-      currentContact: {},
+      recentCalls: [],
+      currentContact: {}, // selectedContact
     }
   },
 
